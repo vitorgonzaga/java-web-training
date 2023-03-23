110c8e8445e1e3466c53cca4da073b7bf1b7b3fc
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-23 08:49:10 -0300

    refactor(UnicaEntradaServlet): implements the "command" design pattern and Reflections (Class) to improve the code.

--- src/main/servlet/UnicaEntradaServlet.java
+++ src/main/servlet/UnicaEntradaServlet.java
@@ -2,6 +2,7 @@ package main.servlet;
 
 import main.acao.*;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,23 +17,52 @@ public class UnicaEntradaServlet extends HttpServlet {
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	
 		String paramAcao = req.getParameter("acao");
-		
-		if(paramAcao.equals("listaempresas")) {
-			ListaEmpresas acao = new ListaEmpresas();
-			acao.executa(req, resp);
-		} else if (paramAcao.equals("removeempresa")) {
-			RemoveEmpresa acao = new RemoveEmpresa();
-			acao.executa(req, resp);
-		} else if (paramAcao.equals("mostraempresa")) {
-			MostraEmpresa acao = new MostraEmpresa();
-			acao.executa(req, resp);
-		} else if (paramAcao.equals("alteraempresa")) {
-			AlteraEmpresa acao = new AlteraEmpresa();
-			acao.executa(req,resp);
-		} else if (paramAcao.equals("novaempresa")) {
-			NovaEmpresa acao = new NovaEmpresa();
-			acao.executa(req, resp);
+		System.out.println(paramAcao);
+
+		String nomeDaClasse = "main.acao." + paramAcao;
+
+		String path = null;
+		try {
+			Class classe = Class.forName(nomeDaClasse);
+			Acao acao = (Acao) classe.newInstance();
+			path = acao.executa(req, resp);
+		} catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+			throw new ServletException(e);
 		}
+
+		String[] tipoEEndereco = path.split(":");
+
+		if(tipoEEndereco[0].equals("forward")) {
+			RequestDispatcher requestDispatcher = req.getRequestDispatcher("WEB-INF/view/" + tipoEEndereco[1]);
+			requestDispatcher.forward(req, resp);
+		} else {
+			resp.sendRedirect(tipoEEndereco[1]);
+		}
+
+//		String path = null;
+//
+//		if(paramAcao.equals("listaempresas")) {
+//			ListaEmpresas acao = new ListaEmpresas();
+//			path = acao.executa(req, resp);
+//		} else if (paramAcao.equals("removeempresa")) {
+//			RemoveEmpresa acao = new RemoveEmpresa();
+//			path = acao.executa(req, resp);
+//		} else if (paramAcao.equals("mostraempresa")) {
+//			MostraEmpresa acao = new MostraEmpresa();
+//			path = acao.executa(req, resp);
+//		} else if (paramAcao.equals("alteraempresa")) {
+//			AlteraEmpresa acao = new AlteraEmpresa();
+//			path = acao.executa(req,resp);
+//		} else if (paramAcao.equals("novaempresa")) {
+//			NovaEmpresa acao = new NovaEmpresa();
+//			path = acao.executa(req, resp);
+//		} else if (paramAcao.equals("novaempresaform")) {
+//			NovaEmpresaForm acao = new NovaEmpresaForm();
+//			path = acao.executa(req, resp);
+//		}
+
+
+
 
 	}
 
